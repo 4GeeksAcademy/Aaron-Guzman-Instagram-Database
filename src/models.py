df44c7b47a1814487078528b8c6a8611f0757146
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-11-15 23:45:23 +0000

    Simple Instagram Database done

--- src/models.py
+++ src/models.py
@@ -1,6 +1,4 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -9,8 +7,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table user
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)  
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
@@ -19,16 +15,20 @@ class User(Base):
     birthday = Column(String(10), nullable=False)
     facebook_account = Column(String(30))
     followers = Column(String(30), nullable=False)
-    posts = relationship("Post")
+    posts = relationship("Post", backref="user")
+    messages = relationship("Message")
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
     date = Column(String(250))
     location = Column(String(250))
     like = Column(String(250), nullable=False)
-    description = Column(String(250), ForeignKey('user.id'))
-    user = relationship("User", back_populates="posts")
+    description = Column(String(250))
+    user = relationship("User")
+    pictures = relationship("Picture")
+    comments = relationship("Comment")
 
 class Update(Base):
     __tablename__ = 'update'
@@ -37,7 +37,9 @@ class Update(Base):
     viewer = Column(String(250))
     reaction = Column(String(250), nullable=False)
     description = Column(String(250), ForeignKey('user.id'))
-    user = relationship("User", back_populates="updates")
+    pictures = relationship("Picture", backref="update")
+    comments = relationship("Comment")
+
 
 class Message(Base):
     __tablename__ = 'message'
@@ -45,16 +47,32 @@ class Message(Base):
     text = Column(Text, nullable=False)
     reaction = Column(String(5))
     date = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
    
+class Picture(Base):
+    __tablename__ = 'picture'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    update_id = Column(Integer, ForeignKey('update.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+    like = Column(Boolean)
+    date = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    update_id = Column(Integer, ForeignKey('update.id'))
 
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
